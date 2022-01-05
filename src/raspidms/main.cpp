503ebf61a872a8e1626091413ecefc6f93344f3f
Lawrencio Rasamoelison <lawrencio.rasamoelison@faurecia.com>
2022-01-05 15:20:13 +0100

    Add correct argument parsing

--- src/raspidms/main.cpp
+++ src/raspidms/main.cpp
@@ -6,24 +6,21 @@
 #include <opencv2/dnn.hpp>
 #include <opencv2/core/utility.hpp>
 
-#include <time.h>
+#include <getopt.h>
 #include <iostream>
-#include <string>
-#include <sys/stat.h>
 #include <errno.h>
 #include <math.h>
 #include <memory>
+#include <string>
+#include <sys/stat.h>
 #include <thread>
-#include <vector>
 #include <utility>
+#include <vector>
 
 #include "Utils.h"
 
 #include "DetectFaces/DetectFacesStage.h"
 #include "FaceFeatures/FaceFeaturesStage.h"
-
-//#include "FaceFeaturesDlib.h"
-
 
 #include "ThreadPool.h"
 #include "SharedQueue.h"
@@ -33,32 +30,86 @@
 const uint32_t MAX_IN_BUFFER_SIZE = 8;
 
 void printHelp () {
-    std::cout << "raspidms [0|PATH_TO_VIDEO.mp4] [haar|tflite|resnetCaffe|yoloResnet18|yoloEffnetb0] [multithread]" << std::endl;
+    std::cout << "USAGE: " << std::endl
+    << "raspidms OPTIONS" << std::endl
+    << "with OPTIONS being ([] are optionals, rest is mandatory. A|B means A or B) :" << std::endl
+    << "    -d|--face-detector haar|tflite|resnetCaffe|yoloResnet18|yoloEffnetb0" << std::endl
+    << "    -m|--face-mesh dlib_68" << std::endl
+    << "    [-j|--multithread]" << std::endl
+    << "    [-h|--help]" << std::endl
+    << "    0|PATH_TO_VIDEO.mp4" << std::endl;
 }
 
 typedef std::function<std::pair<std::vector<cv::Rect>, double>(cv::Mat frame)> DetectFaceFunc;
 
-int main(int argc, char**argv) {
-    if (argc < 3) {
-        printHelp();
-        return 0;
+struct Args {
+    std::string video_path;
+    std::string face_detector_model;
+    std::string face_mesh_model;
+    bool multithread;
+};
+
+struct Args parseArgs(int argc, char** argv) {
+
+    struct Args args;
+    args.multithread = false;
+
+    //Specifying the expected options
+    //The two options l and b expect numbers as argument
+    static struct option long_options[] = {
+    {"face-detector",  required_argument,  0,  'd' },
+    {"face-mesh",      required_argument,  0,  'm' },
+    {"multithread",    no_argument,        0,  'j' },
+    {"help",           no_argument,        0,  'h' },
+    {0, 0, 0, 0},
+    };
+
+    char opt = 0;
+    int long_index = 0;
+    while ((opt = getopt_long(argc, argv, "d:m:jh",
+                              long_options, &long_index )) != -1) {
+        switch (opt) {
+            case 'd' :
+                args.face_detector_model = std::string(optarg);
+                break;
+            case 'm' :
+                args.face_mesh_model = std::string(optarg);
+                break;
+            case 'j':
+                args.multithread = true;
+                break;
+            case 'h':
+                printHelp();
+                exit(EXIT_SUCCESS);
+            default:
+                printHelp();
+                exit(EXIT_FAILURE);
+        }
     }
 
-    const std::string video_path(argv[1]);
-    const std::string firstDetector = argc > 2 ? argv[2] : "";
-    const bool multithread = argc > 3 ? (std::string(argv[3]) == "multithread" ? true : false) : false;
+    if (optind < argc && argc - optind == 1) {
+        args.video_path = std::string(argv[argc - 1]);
+    } else {
+        printHelp();
+        exit(EXIT_FAILURE);
+    }
 
+    return args;
+}
+
+int main(int argc, char**argv) {
+    const struct Args args = parseArgs(argc, argv);
 
     cv::VideoCapture cap;
 
-    if (video_path == "0") {
-        cap.open(0);
+    if (!args.video_path.empty() && std::all_of(args.video_path.begin(), args.video_path.end(), ::isdigit)) {
+        cap.open(stoi(args.video_path));
     } else {
-        cap.open(video_path);
+        cap.open(args.video_path);
     }
 
     if (! cap.isOpened()) {
-        std::cerr << "Can't open file " << video_path << std::endl;
+        std::cerr << "Can't open file " << args.video_path << std::endl;
     }
 
     std::cout << "Start grabbing" << std::endl;
@@ -73,10 +124,10 @@ int main(int argc, char**argv) {
     std::shared_ptr<SharedQueue<PointsList>> faceFeaturesQueue(new SharedQueue<PointsList>());
 
     // Responsible of detecting faces, needs in frames, and ouputs out rectangles
-    DetectFacesStage detectFacesStage(firstDetector, inputFrameQueue, rectsQueue);
+    DetectFacesStage detectFacesStage(args.face_detector_model, inputFrameQueue, rectsQueue);
 
     // Responsible for detecting face feature (landmarks)
-    FaceFeaturesStage faceFeaturesStage("dlib_68", inputFrameQueue, rectsQueue, faceFeaturesQueue);
+    FaceFeaturesStage faceFeaturesStage(args.face_mesh_model, inputFrameQueue, rectsQueue, faceFeaturesQueue);
 
     Scheduler scheduler;
 
@@ -111,7 +162,7 @@ int main(int argc, char**argv) {
         //TODO consider no copy
         inputFrameQueue->push_back(frame);
 
-        if (multithread) {
+        if (args.multithread) {
             scheduler.schedule();
         } else {
             detectFacesStage(0);
